a057e2ce727f79a8b26bc45a423ee9acb08a6b90
Anil Shanker <anilshanker@hotmail.com>
2021-09-03 11:47:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flicks/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.flicks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+// This is a test to make sure Git and Github are working
